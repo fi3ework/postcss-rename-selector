8d002bdff9744c59dd69d0507f619e790a68922b
fi3ework <fi3ework@gmail.com>
2020-04-06 19:14:44 +0800

    fix: preserve before and after steady

--- src/presets/antd.ts
+++ src/presets/antd.ts
@@ -2,41 +2,43 @@ import parser, { Node } from 'postcss-selector-parser'
 import { SelectorReplace } from '../index'
 
 export function antdScopeReplacerFn(node: Node) {
-  if (node.type === 'selector') {
-    const firstAntClassNodeIndex = node.nodes.findIndex((n) => {
-      return n.type === 'class' && n.value.startsWith('ant-')
-    })
-    if (firstAntClassNodeIndex < 0) return
+  if (node.type !== 'selector') return
 
-    // preserve line break
-    const firstAntClassNode = node.nodes[firstAntClassNodeIndex]
-    const prevNode = node.nodes[firstAntClassNodeIndex - 1]
-    const before = firstAntClassNode.rawSpaceBefore
-    firstAntClassNode.setPropertyWithoutEscape('rawSpaceBefore', '')
+  const firstAntClassNodeIndex = node.nodes.findIndex((n) => {
+    return n.type === 'class' && n.value.startsWith('ant-')
+  })
+  if (firstAntClassNodeIndex < 0) return
 
-    const toInsert = []
-    if (firstAntClassNodeIndex === 0 || prevNode.type === 'combinator') {
-      const universal = parser.universal({
-        value: '*',
-        spaces: {
-          before: before,
-          after: '',
-        },
-      })
-      toInsert.push(universal)
-    }
+  const firstAntClassNode = node.nodes[firstAntClassNodeIndex]
+  const prevNode = node.nodes[firstAntClassNodeIndex - 1]
 
-    const attr = parser.attribute({
-      attribute: 'class',
-      operator: '*=',
-      value: `"ant-"`,
-      raws: {},
-    })
-
-    toInsert.push(attr)
-
-    firstAntClassNode.parent!.nodes.splice(firstAntClassNodeIndex, 0, ...toInsert)
+  // preserve line break
+  const spaces = {
+    before: firstAntClassNode.rawSpaceBefore,
+    after: firstAntClassNode.rawSpaceAfter,
   }
+
+  firstAntClassNode.setPropertyWithoutEscape('rawSpaceBefore', '')
+  const toInsert = []
+
+  if (firstAntClassNodeIndex === 0 || prevNode.type === 'combinator') {
+    const universal = parser.universal({
+      value: '*',
+    })
+    toInsert.push(universal)
+  }
+
+  const attr = parser.attribute({
+    attribute: 'class',
+    operator: '*=',
+    value: `"ant-"`,
+    raws: {},
+  })
+
+  toInsert.push(attr)
+  toInsert[0].spaces = spaces
+
+  firstAntClassNode.parent!.nodes.splice(firstAntClassNodeIndex, 0, ...toInsert)
 }
 
 export const antdReplacer: SelectorReplace = {
